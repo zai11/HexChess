0910420ce19a8b4c4b5799c390a9b3e155cbbda2
Zaichic Turner <zaipowell@rocketmail.com>
2023-07-27 00:51:52 +1000

    Added unit tests for Rook class

--- tests/test.Knight.js
+++ tests/test.Knight.js
@@ -11,48 +11,48 @@ describe('Knight Class Tests:', () => {
         it('F7, not blocked should return [C5, C6, D8, E9, G10, H10, I9, I8, H6, G5, E4, D4]', () => {
             let expected = ['C5', 'C6', 'D8', 'E9', 'G10', 'H10', 'I9', 'I8', 'H6', 'G5', 'E4', 'D4'];
             board.addPiece(new Knight(board, 'F7', 'white'));
-            let bishop = board.getPieceFromCoord('F7');
-            let result = bishop.getValidMoves();
+            let knight = board.getPieceFromCoord('F7');
+            let result = knight.getValidMoves();
             expect(result).to.equal(expected);
         });
         it('F7, blocked by own piece on G10 should return [C5, C6, D8, E9, H10, I9, I8, H6, G5, E4, D4]', () => {
             let expected = ['C5', 'C6', 'D8', 'E9', 'H10', 'I9', 'I8', 'H6', 'G5', 'E4', 'D4'];
             board.addPiece(new Knight(board, 'F7', 'white'));
             board.addPiece(new Pawn(board, 'G10', 'white'));
-            let bishop = board.getPieceFromCoord('F7');
-            let result = bishop.getValidMoves();
+            let knight = board.getPieceFromCoord('F7');
+            let result = knight.getValidMoves();
             expect(result).to.equal(expected);
         });
         it('F7, blocked by enemy on G10 should return [C5, C6, D8, E9, G10, H10, I9, I8, H6, G5, E4, D4]', () => {
             let expected = ['C5', 'C6', 'D8', 'E9', 'G10', 'H10', 'I9', 'I8', 'H6', 'G5', 'E4', 'D4'];
             board.addPiece(new Knight(board, 'F7', 'white'));
-            let bishop = board.getPieceFromCoord('F7');
             board.addPiece(new Pawn(board, 'G10', 'black'));
-            let result = bishop.getValidMoves();
+            let knight = board.getPieceFromCoord('F7');
+            let result = knight.getValidMoves();
             expect(result).to.equal(expected);
         });
 
         it('I7, not blocked should return [F5, F6, G8, H9, J10, K10, K6, J5, H4, G4]', () => {
             let expected = ['F5', 'F6', 'G8', 'H9', 'J10', 'K10', 'K6', 'J5', 'H4', 'G4'];
             board.addPiece(new Knight(board, 'I7', 'white'));
-            let bishop = board.getPieceFromCoord('I7');
-            let result = bishop.getValidMoves();
+            let knight = board.getPieceFromCoord('I7');
+            let result = knight.getValidMoves();
             expect(result).to.equal(expected);
         });
         it('I7, blocked by own piece on G4 should return [F5, F6, G8, H9, J10, K10, K6, J5, H4]', () => {
             let expected = ['F5', 'F6', 'G8', 'H9', 'J10', 'K10', 'K6', 'J5', 'H4'];
             board.addPiece(new Knight(board, 'I7', 'white'));
             board.addPiece(new Pawn(board, 'G4', 'white'));
-            let bishop = board.getPieceFromCoord('I7');
-            let result = bishop.getValidMoves();
+            let knight = board.getPieceFromCoord('I7');
+            let result = knight.getValidMoves();
             expect(result).to.equal(expected);
         });
         it('I7, blocked by enemy on G4 should return [F5, F6, G8, H9, J10, K10, K6, J5, H4]', () => {
             let expected = ['F5', 'F6', 'G8', 'H9', 'J10', 'K10', 'K6', 'J5', 'H4'];
             board.addPiece(new Knight(board, 'I7', 'white'));
             board.addPiece(new Pawn(board, 'G4', 'black'));
-            let bishop = board.getPieceFromCoord('I7');
-            let result = bishop.getValidMoves();
+            let knight = board.getPieceFromCoord('I7');
+            let result = knight.getValidMoves();
             expect(result).to.equal(expected);
         });
     });
---
+++ tests/test.Rook.js
@@ -0,0 +1,64 @@
+import { Board } from "../src/Board.js";
+import { Pawn, Rook } from '../src/Piece.js';
+import { tiles_data, coords_data } from "./test.data.js";
+
+let expect = chai.expect;
+
+let board = new Board(undefined, tiles_data, coords_data);
+
+describe('Rook Class Tests:', () => {
+    describe('getValidMoves()', () => {
+        it('F7, not blocked should return [E7, D7, C7, B7, F8, F9, F10, F11, G8, H9, I10, J11, G7, H7, I7, J7, K7, F6, F5, F4, F3, F2, F1, E6, D5, C4, B3, A2]', () => {
+            let expected = ['E7', 'D7', 'C7', 'B7', 'F8', 'F9', 'F10', 'F11', 'G8', 'H9', 'I10', 'J11', 'G7', 'H7',
+                'I7', 'J7', 'K7', 'F6', 'F5', 'F4', 'F3', 'F2', 'F1', 'E6', 'D5', 'C4', 'B3', 'A2'];
+            board.addPiece(new Rook(board, 'F7', 'white'));
+            let rook = board.getPieceFromCoord('F7');
+            let result = rook.getValidMoves();
+            expect(result).to.equal(expected);
+        });
+        it('F7, blocked by own piece on F3 should return [E7, D7, C7, B7, F8, F9, F10, F11, G8, H9, I10, J11, G7, H7, I7, J7, K7, F6, F5, F4, E6, D5, C4, B3, A2]', () => {
+            let expected = ['E7', 'D7', 'C7', 'B7', 'F8', 'F9', 'F10', 'F11', 'G8', 'H9', 'I10', 'J11', 'G7', 'H7',
+                'I7', 'J7', 'K7', 'F6', 'F5', 'F4', 'E6', 'D5', 'C4', 'B3', 'A2'];
+            board.addPiece(new Rook(board, 'F7', 'white'));
+            board.addPiece(new Pawn(board, 'F3', 'white'));
+            let rook = board.getPieceFromCoord('F7');
+            let result = rook.getValidMoves();
+            expect(result).to.equal(expected);
+        });
+        it('F7, blocked by enemy on F3 should return [E7, D7, C7, B7, F8, F9, F10, F11, G8, H9, I10, J11, G7, H7, I7, J7, K7, F6, F5, F4, F3, E6, D5, C4, B3, A2]', () => {
+            let expected = ['E7', 'D7', 'C7', 'B7', 'F8', 'F9', 'F10', 'F11', 'G8', 'H9', 'I10', 'J11', 'G7', 'H7',
+                'I7', 'J7', 'K7', 'F6', 'F5', 'F4', 'F3', 'E6', 'D5', 'C4', 'B3', 'A2'];
+            board.addPiece(new Rook(board, 'F7', 'white'));
+            board.addPiece(new Pawn(board, 'F3', 'black'));
+            let rook = board.getPieceFromCoord('F7');
+            let result = rook.getValidMoves();
+            expect(result).to.equal(expected);
+        });
+
+        it('C5, not blocked should return [B5, A5, C6, C7, C8, D6, E7, F8, G9, H10, I11, D5, E5, F5, G5, H5, I5, J5, C4, C3, C2, C1, B4, A3]', () => {
+            let expected = ['B5', 'A5', 'C6', 'C7', 'C8', 'D6', 'E7', 'F8', 'G9', 'H10', 'I11', 'D5', 'E5', 'F5', 'G5',
+                'H5', 'I5', 'J5', 'C4', 'C3', 'C2', 'C1', 'B4', 'A3'];
+            board.addPiece(new Rook(board, 'C5', 'white'));
+            let rook = board.getPieceFromCoord('C5');
+            let result = rook.getValidMoves();
+            expect(result).to.equal(expected);
+        });
+        it('C5, blocked by own piece on E5 should return [B5, A5, C6, C7, C8, D6, E7, F8, G9, H10, I11, D5, C4, C3, C2, C1, B4, A3]', () => {
+            let expected = ['B5', 'A5', 'C6', 'C7', 'C8', 'D6', 'E7', 'F8', 'G9', 'H10', 'I11', 'D5', 'C4', 'C3', 'C2',
+                'C1', 'B4', 'A3'];
+            board.addPiece(new Rook(board, 'C5', 'white'));
+            board.addPiece(new Pawn(board, 'E5', 'white'));
+            let rook = board.getPieceFromCoord('C5');
+            let result = rook.getValidMoves();
+            expect(result).to.equal(expected);
+        });
+        it('C5, blocked by enemy on E5 should return [B5, A5, C6, C7, C8, D6, E7, F8, G9, H10, I11, D5, E5, C4, C3, C2, C1, B4, A3]', () => {
+            let expected = ['B5', 'A5', 'C6', 'C7', 'C8', 'D6', 'E7', 'F8', 'G9', 'H10', 'I11', 'D5', 'E5', 'C4', 'C3', 'C2', 'C1', 'B4', 'A3'];
+            board.addPiece(new Rook(board, 'C5', 'white'));
+            board.addPiece(new Pawn(board, 'E5', 'black'));
+            let rook = board.getPieceFromCoord('C5');
+            let result = rook.getValidMoves();
+            expect(result).to.equal(expected);
+        });
+    });
+});
