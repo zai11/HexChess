b5405251861ff10dd7e5925954fb1634e26610b0
Zaichic Turner <zaipowell@rocketmail.com>
2023-08-07 00:50:35 +1000

    Added functionality for getValidMoves method of Rook class

--- src/Rook.js
+++ src/Rook.js
@@ -6,4 +6,36 @@ export class Rook extends Piece {
         let positions = board.getPositionsFromCoord(coordinate)
         super(board, coordinate, colour, positions.x, positions.y, sprite);
     }
+
+    getValidMoves = (context, boundary_data) => {
+        let validMoves = [];
+        let currentTile = this.board.getTileFromPositions(this.x, this.y);
+
+        let directions = ['ForwardLeft', 'Forward', 'ForwardRight', 'BackwardRight', 'Backward', 'BackwardLeft']
+
+        // While there is a leftDiagonal
+        directions.forEach((direction) => {
+            let nextTile = eval('this.board.getTileFromCoord(currentTile.get' + direction + '(context, boundary_data))');
+            while (nextTile !== undefined) {
+                // if it's blocked by an enemy piece
+                if (nextTile.hasPiece() && nextTile.getPiece().colour !== this.colour) {
+                    validMoves.push(nextTile.coordinate);
+                    break;
+                }
+    
+                // if it's blocked by a friendly piece
+                if (nextTile.hasPiece() && nextTile.getPiece().colour === this.colour) {
+                    break;
+                }
+    
+                // if it's not blocked
+                if (!nextTile.hasPiece())
+                    validMoves.push(nextTile.coordinate);
+    
+                nextTile = eval('this.board.getTileFromCoord(nextTile.get' + direction + '(context, boundary_data))');
+            }
+        });
+
+        return validMoves;
+    }
 }
--- tests/test.Rook.js
+++ tests/test.Rook.js
@@ -1,10 +1,11 @@
 import { Board } from "../src/Board.js";
-import { Pawn, Rook } from '../src/Piece.js';
-import { tiles_data, coords_data } from "./test.data.js";
+import { Rook } from "../src/Rook.js";
+import { Pawn } from '../src/Pawn.js';
+import { tiles_data_white, coords_data_white, boundary_data } from "./test.data.js";
 
 let expect = chai.expect;
 
-let board = new Board(undefined, tiles_data, coords_data);
+let board = new Board(undefined, 'white', tiles_data_white, coords_data_white);
 
 describe('Rook Class Tests:', () => {
     describe('getValidMoves()', () => {
@@ -13,8 +14,8 @@ describe('Rook Class Tests:', () => {
                 'I7', 'J7', 'K7', 'F6', 'F5', 'F4', 'F3', 'F2', 'F1', 'E6', 'D5', 'C4', 'B3', 'A2'];
             board.addPiece(new Rook(board, 'F7', 'white'));
             let rook = board.getPieceFromCoord('F7');
-            let result = rook.getValidMoves();
-            expect(result).to.equal(expected);
+            let result = rook.getValidMoves(undefined, boundary_data);
+            expect(result).deep.to.equal(expected);
         });
         it('F7, blocked by own piece on F3 should return [E7, D7, C7, B7, F8, F9, F10, F11, G8, H9, I10, J11, G7, H7, I7, J7, K7, F6, F5, F4, E6, D5, C4, B3, A2]', () => {
             let expected = ['E7', 'D7', 'C7', 'B7', 'F8', 'F9', 'F10', 'F11', 'G8', 'H9', 'I10', 'J11', 'G7', 'H7',
@@ -22,8 +23,8 @@ describe('Rook Class Tests:', () => {
             board.addPiece(new Rook(board, 'F7', 'white'));
             board.addPiece(new Pawn(board, 'F3', 'white'));
             let rook = board.getPieceFromCoord('F7');
-            let result = rook.getValidMoves();
-            expect(result).to.equal(expected);
+            let result = rook.getValidMoves(undefined, boundary_data);
+            expect(result).deep.to.equal(expected);
         });
         it('F7, blocked by enemy on F3 should return [E7, D7, C7, B7, F8, F9, F10, F11, G8, H9, I10, J11, G7, H7, I7, J7, K7, F6, F5, F4, F3, E6, D5, C4, B3, A2]', () => {
             let expected = ['E7', 'D7', 'C7', 'B7', 'F8', 'F9', 'F10', 'F11', 'G8', 'H9', 'I10', 'J11', 'G7', 'H7',
@@ -31,8 +32,8 @@ describe('Rook Class Tests:', () => {
             board.addPiece(new Rook(board, 'F7', 'white'));
             board.addPiece(new Pawn(board, 'F3', 'black'));
             let rook = board.getPieceFromCoord('F7');
-            let result = rook.getValidMoves();
-            expect(result).to.equal(expected);
+            let result = rook.getValidMoves(undefined, boundary_data);
+            expect(result).deep.to.equal(expected);
         });
 
         it('C5, not blocked should return [B5, A5, C6, C7, C8, D6, E7, F8, G9, H10, I11, D5, E5, F5, G5, H5, I5, J5, C4, C3, C2, C1, B4, A3]', () => {
@@ -40,8 +41,8 @@ describe('Rook Class Tests:', () => {
                 'H5', 'I5', 'J5', 'C4', 'C3', 'C2', 'C1', 'B4', 'A3'];
             board.addPiece(new Rook(board, 'C5', 'white'));
             let rook = board.getPieceFromCoord('C5');
-            let result = rook.getValidMoves();
-            expect(result).to.equal(expected);
+            let result = rook.getValidMoves(undefined, boundary_data);
+            expect(result).deep.to.equal(expected);
         });
         it('C5, blocked by own piece on E5 should return [B5, A5, C6, C7, C8, D6, E7, F8, G9, H10, I11, D5, C4, C3, C2, C1, B4, A3]', () => {
             let expected = ['B5', 'A5', 'C6', 'C7', 'C8', 'D6', 'E7', 'F8', 'G9', 'H10', 'I11', 'D5', 'C4', 'C3', 'C2',
@@ -49,16 +50,16 @@ describe('Rook Class Tests:', () => {
             board.addPiece(new Rook(board, 'C5', 'white'));
             board.addPiece(new Pawn(board, 'E5', 'white'));
             let rook = board.getPieceFromCoord('C5');
-            let result = rook.getValidMoves();
-            expect(result).to.equal(expected);
+            let result = rook.getValidMoves(undefined, boundary_data);
+            expect(result).deep.to.equal(expected);
         });
         it('C5, blocked by enemy on E5 should return [B5, A5, C6, C7, C8, D6, E7, F8, G9, H10, I11, D5, E5, C4, C3, C2, C1, B4, A3]', () => {
             let expected = ['B5', 'A5', 'C6', 'C7', 'C8', 'D6', 'E7', 'F8', 'G9', 'H10', 'I11', 'D5', 'E5', 'C4', 'C3', 'C2', 'C1', 'B4', 'A3'];
             board.addPiece(new Rook(board, 'C5', 'white'));
             board.addPiece(new Pawn(board, 'E5', 'black'));
             let rook = board.getPieceFromCoord('C5');
-            let result = rook.getValidMoves();
-            expect(result).to.equal(expected);
+            let result = rook.getValidMoves(undefined, boundary_data);
+            expect(result).deep.to.equal(expected);
         });
     });
 });
