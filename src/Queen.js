7f58d531b0a0a88f0d79adabbffd328f412394c9
Zaichic Turner <zaipowell@rocketmail.com>
2023-08-07 01:02:58 +1000

    Added functionality for getValidMoves method of Queen class

--- src/Queen.js
+++ src/Queen.js
@@ -6,4 +6,37 @@ export class Queen extends Piece {
         let positions = board.getPositionsFromCoord(coordinate)
         super(board, coordinate, colour, positions.x, positions.y, sprite);
     }
+
+    getValidMoves = (context, boundary_data) => {
+        let validMoves = [];
+        let currentTile = this.board.getTileFromPositions(this.x, this.y);
+
+        let directions = ['LeftDiagonal', 'ForwardLeft', 'ForwardLeftDiagonal', 'Forward', 'ForwardRightDiagonal', 
+            'ForwardRight', 'RightDiagonal', 'BackwardRight', 'BackwardRightDiagonal', 'Backward', 'BackwardLeftDiagonal',
+            'BackwardLeft'];
+
+        directions.forEach((direction) => {
+            let nextTile = eval('this.board.getTileFromCoord(currentTile.get' + direction + '(context, boundary_data))');
+            while (nextTile !== undefined) {
+                // if it's blocked by an enemy piece
+                if (nextTile.hasPiece() && nextTile.getPiece().colour !== this.colour) {
+                    validMoves.push(nextTile.coordinate);
+                    break;
+                }
+    
+                // if it's blocked by a friendly piece
+                if (nextTile.hasPiece() && nextTile.getPiece().colour === this.colour) {
+                    break;
+                }
+    
+                // if it's not blocked
+                if (!nextTile.hasPiece())
+                    validMoves.push(nextTile.coordinate);
+    
+                nextTile = eval('this.board.getTileFromCoord(nextTile.get' + direction + '(context, boundary_data))');
+            }
+        });
+
+        return validMoves;
+    }
 }
--- tests/test.Queen.js
+++ tests/test.Queen.js
@@ -1,39 +1,40 @@
 import { Board } from "../src/Board.js";
-import { Pawn, Queen } from '../src/Piece.js';
-import { tiles_data, coords_data } from "./test.data.js";
+import { Queen } from "../src/Queen.js";
+import { Pawn } from '../src/Pawn.js';
+import { tiles_data_white, coords_data_white, boundary_data } from "./test.data.js";
 
 let expect = chai.expect;
 
-let board = new Board(undefined, tiles_data, coords_data);
+let board = new Board(undefined, 'white', tiles_data_white, coords_data_white);
 
 describe('Queen Class Tests:', () => {
     describe('getValidMoves()', () => {
         it('F7, not blocked should return [D6, B5, E7, D7, C7, B7, E8, D9, F8, F9, F10, F11, G9, H11, G8, H9, I10, J11, H8, J9, G7, H7, I7, J7, K7, G6, H5, I4, F6, F5, F4, F3, F2, F1, E5, D3, C1, E6, D5, C4, B3, A2]', () => {
-            let expected = ['D6', 'B5', 'E7', 'D7', 'C7', 'B7', 'E8', 'D9', 'F8', 'F10', 'F11', 'G9', 'H11', 'G8', 'H9',
+            let expected = ['D6', 'B5', 'E7', 'D7', 'C7', 'B7', 'E8', 'D9', 'F8', 'F9', 'F10', 'F11', 'G9', 'H11', 'G8', 'H9',
                 'I10', 'J11', 'H8', 'J9', 'G7', 'H7', 'I7', 'J7', 'K7', 'G6', 'H5', 'I4', 'F6', 'F5', 'F4', 'F3', 'F2', 
                 'F1', 'E5', 'D3', 'C1', 'E6', 'D5', 'C4', 'B3', 'A2'];
             board.addPiece(new Queen(board, 'F7', 'white'));
             let queen = board.getPieceFromCoord('F7');
-            let result = queen.getValidMoves();
-            expect(result).to.equal(expected);
+            let result = queen.getValidMoves(undefined, boundary_data);
+            expect(result).deep.to.equal(expected);
         });
         it('F7, blocked by own piece on F6 should return [D6, B5, E7, D7, C7, B7, E8, D9, F8, F9, F10, F11, G9, H11, G8, H9, I10, J11, H8, J9, G7, H7, I7, J7, K7, G6, H5, I4, E5, D3, C1, E6, D5, C4, B3, A2]', () => {
-            let expected = ['D6', 'B5', 'E7', 'D7', 'C7', 'B7', 'E8', 'D9', 'F8', 'F10', 'F11', 'G9', 'H11', 'G8', 'H9',
+            let expected = ['D6', 'B5', 'E7', 'D7', 'C7', 'B7', 'E8', 'D9', 'F8', 'F9', 'F10', 'F11', 'G9', 'H11', 'G8', 'H9',
                 'I10', 'J11', 'H8', 'J9', 'G7', 'H7', 'I7', 'J7', 'K7', 'G6', 'H5', 'I4', 'E5', 'D3', 'C1', 'E6', 'D5', 'C4', 'B3', 'A2'];
             board.addPiece(new Queen(board, 'F7', 'white'));
             board.addPiece(new Pawn(board, 'F6', 'white'));
             let queen = board.getPieceFromCoord('F7');
-            let result = queen.getValidMoves();
-            expect(result).to.equal(expected);
+            let result = queen.getValidMoves(undefined, boundary_data);
+            expect(result).deep.to.equal(expected);
         });
         it('F7, blocked by enemy on F6 should return [D6, B5, E7, D7, C7, B7, E8, D9, F8, F9, F10, F11, G9, H11, G8, H9, I10, J11, H8, J9, G7, H7, I7, J7, K7, G6, H5, I4, F6, E5, D3, C1, E6, D5, C4, B3, A2]', () => {
-            let expected = ['D6', 'B5', 'E7', 'D7', 'C7', 'B7', 'E8', 'D9', 'F8', 'F10', 'F11', 'G9', 'H11', 'G8', 'H9',
+            let expected = ['D6', 'B5', 'E7', 'D7', 'C7', 'B7', 'E8', 'D9', 'F8', 'F9', 'F10', 'F11', 'G9', 'H11', 'G8', 'H9',
                 'I10', 'J11', 'H8', 'J9', 'G7', 'H7', 'I7', 'J7', 'K7', 'G6', 'H5', 'I4', 'F6', 'E5', 'D3', 'C1', 'E6', 'D5', 'C4', 'B3', 'A2'];
             board.addPiece(new Queen(board, 'F7', 'white'));
             board.addPiece(new Pawn(board, 'F6', 'black'));
             let queen = board.getPieceFromCoord('F7');
-            let result = queen.getValidMoves();
-            expect(result).to.equal(expected);
+            let result = queen.getValidMoves(undefined, boundary_data);
+            expect(result).deep.to.equal(expected);
         });
     });
 });
